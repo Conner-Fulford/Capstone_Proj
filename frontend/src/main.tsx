5372551cc1286d794a618a818d717f3b386f906a
Jay3636 <58828215+Jay3636@users.noreply.github.com>
2024-02-26 16:06:48 -0500

    added account management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import ResponsiveRendering from './components/ResponsiveRendering';
+import AccManage from './components/AccManage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <ResponsiveRendering />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <AccManage />
+  </React.StrictMode>,
 );
-
