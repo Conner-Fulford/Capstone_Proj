6108f94d20c3a3876e4fe1bb5818035565150f03
Chandler <89811827+4am-walking@users.noreply.github.com>
2024-02-28 19:04:08 +0000

    security best practices according to express:
    https://expressjs.com/en/advanced/best-practice-security.html#use-tls

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,44 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import path from 'path';
 
 dotenv.config();
 
+// Functionality
 const authRoutes = require("./routes/authRoutes");
 const app: Application = express();
 const db: any = require('./config/db');
-const helmet = require("helmet");
 const port: string | number = process.env.PORT || 8080;
+
+// Security
+const helmet = require('helmet');
+const morgan = require("morgan");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(morgan("dev"));
+app.disable('x-powered-by');
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+app.use(session({
+    name: "authCookie",
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      secure: false,
+      maxAge: 60000
+    }
+  })
+);
+app.use(cookieParser());
 app.use('/', authRoutes);
-app.get('/test', async (req, res) => {
+
+
+app.get('/test', async (req: Request, res: Response) => {
   try {
     const result = await db.query('SELECT * FROM users');
     res.json(result.rows);
@@ -27,5 +49,5 @@ app.get('/test', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
