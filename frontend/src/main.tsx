75bf5c3ff267dd75c539e4a32acf7238caa0b5f1
Conner-Fulford <dnf283@mocs.utc.edu>
2024-03-05 20:10:35 -0500

    fixed merge bug in frointend/src/main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-<<<<<<< Updated upstream
-import ResponsiveRendering from './components/ResponsiveRendering';
 import AccManage from './components/AccManage';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AccManage />
-  </React.StrictMode>,
-);
-=======
 import ResponsiveRendering from './components/utilities/ResponsiveRendering';
 import HomePageD from './components/pages/HomePageD';
 import UserPageD from './components/pages/UserPageD';
@@ -21,4 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </React.StrictMode>,
 );
 
->>>>>>> Stashed changes
