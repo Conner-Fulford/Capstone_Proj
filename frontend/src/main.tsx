5ea2bbea5c19b203209c2b2443a6d6c14d97dcaa
Damion Mason <dmason@checkintocash.com>
2024-03-21 16:41:51 -0400

    performing git reset --soft to fix issue for Jay

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import ResponsiveRendering from './components/utilities/ResponsiveRendering';
-import HomePageD from './components/pages/HomePageD';
-// import HomePageM from './components/pages/HomePageM';
-import UserPageD from './components/pages/UserPageD';
-import UserPageM from './components/pages/UserPageM';
-
+import ResponsiveRendering from './components/ResponsiveRendering';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ResponsiveRendering />
     </React.StrictMode>,
 );
+
