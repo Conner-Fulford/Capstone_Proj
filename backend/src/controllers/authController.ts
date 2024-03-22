7f778ed6316e18df707327d346a1f288d0eb7b10
Chandler <89811827+4am-walking@users.noreply.github.com>
2024-03-22 19:25:22 +0000

    initial commit of jwt user_id verification

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -5,10 +5,9 @@ import db from "../config/db";
 const login = async (req: any, res: any) => {
   const { email, password } = req.body;
   try {
-    const data = await db.query(
-      `SELECT * FROM users WHERE email= $1;`,
-      [email]
-    );
+    const data = await db.query(`SELECT * FROM users WHERE email= $1;`, [
+      email,
+    ]);
     const user = data.rows;
     if (user.length === 0) {
       res.status(400).json({
@@ -26,6 +25,7 @@ const login = async (req: any, res: any) => {
           } else if (result === true) {
             const token = jwt.sign(
               {
+                user_id: user[0].user_id,
                 email: email,
               },
               (process.env as any).SECRET_KEY
@@ -54,10 +54,9 @@ const login = async (req: any, res: any) => {
 const register = async (req: any, res: any) => {
   const { email, password } = req.body;
   try {
-    const data = await db.query(
-      `SELECT * FROM users WHERE email= $1;`,
-      [email]
-    );
+    const data = await db.query(`SELECT * FROM users WHERE email= $1;`, [
+      email,
+    ]);
     const arr = data.rows;
     if (arr.length != 0) {
       return res.status(400).json({
@@ -70,17 +69,19 @@ const register = async (req: any, res: any) => {
         password: hash,
       };
       db.query(
-        `INSERT INTO users (email, password) VALUES ($1,$2);`,
+        `INSERT INTO users (email, password) VALUES ($1,$2) RETURNING user_id;`,
         [user.email, user.password],
-        (err: any) => {
+        (err: any, result: any) => {
           if (err) {
             console.error(err);
             return res.status(500).json({
               error: "Database error",
             });
           } else {
+            const user_id = result.rows[0].user_id;
             const token = jwt.sign(
               {
+                user_id: user_id,
                 email: user.email,
               },
               (process.env as any).SECRET_KEY
